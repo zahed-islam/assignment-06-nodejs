8b1b202749300a669ad99e2cc8648a255589cec6
Zahedul Islam <zahedult96@gmail.com>
2026-09-11 19:01:15 +0600

    nvalid Route

--- server.js
+++ server.js
@@ -94,24 +94,24 @@ const server = http.createServer((req, res) => {
 
     }
 
-//     // =========================
-//     // Invalid Route
-//     // =========================
-//     else {
+    // =========================
+    // Invalid Route
+    // =========================
+    else {
 
-//         res.writeHead(404, {
-//             "Content-Type": "text/html"
-//         });
+        res.writeHead(404, {
+            "Content-Type": "text/html"
+        });
 
-//         res.end(`
-//             <h1>404 - Page Not Found</h1>
-//             <p>The page you are looking for does not exist.</p>
+        res.end(`
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
 
-//             <a href="/">Go to Home Page</a>
-//         `);
-//     }
+            <a href="/">Go to Home Page</a>
+        `);
+    }
 
-// });
+});
 
 
 // =========================
